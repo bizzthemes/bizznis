de143e251cab8468a95df8a7de95e211c6f2366a
bizzthemes <info@bizzthemes.com>
2014-06-23 20:10:56 +0200

    Add new call to comments

--- comments.php
+++ comments.php
@@ -5,8 +5,13 @@
 	Please do all modifications in the form of a child theme.
 */
 
+//* Stop here if comments.php file doesn't exist
+if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && 'comments.php' === basename( $_SERVER['SCRIPT_FILENAME'] ) )
+	die ( 'Please do not load this page directly. Thanks!' );
+
 //* Stop here if the current post is protected by a password
 if ( post_password_required() ) {
+	printf( '<p class="alert">%s</p>', __( 'This post is password protected. Enter the password to view comments.', 'bizznis' ) );
 	return;
 }
 
